91139d4f7a3edc8c2e54e06953e8903b0b47827f
gauthamkrishna-2002006 <gauthamkrishnacg@gmail.com>
2026-07-12 21:00:09 +0530

    Add dice rolling animation
    Refactor rollDice function to improve animation and result display.

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
-function rollDice(){
+const dice = document.getElementById("dice");
+const result = document.getElementById("result");
 
-const number=Math.floor(Math.random()*6)+1;
+function rollDice() {
 
-document.getElementById("dice").src=
-"https://upload.wikimedia.org/wikipedia/commons/"+[
-"",
-"1/1b/Dice-1-b.svg",
-"5/5f/Dice-2-b.svg",
-"b/b1/Dice-3-b.svg",
-"f/fd/Dice-4-b.svg",
-"0/08/Dice-5-b.svg",
-"2/26/Dice-6-b.svg"
-][number];
+    dice.style.transform = "rotate(720deg)";
+    dice.style.opacity = "0.5";
 
-document.getElementById("result").innerHTML="You rolled <b>"+number+"</b>";
+    setTimeout(() => {
+
+        const number = Math.floor(Math.random() * 6) + 1;
+
+        dice.src = [
+            "",
+            "https://upload.wikimedia.org/wikipedia/commons/1/1b/Dice-1-b.svg",
+            "https://upload.wikimedia.org/wikipedia/commons/5/5f/Dice-2-b.svg",
+            "https://upload.wikimedia.org/wikipedia/commons/b/b1/Dice-3-b.svg",
+            "https://upload.wikimedia.org/wikipedia/commons/f/fd/Dice-4-b.svg",
+            "https://upload.wikimedia.org/wikipedia/commons/0/08/Dice-5-b.svg",
+            "https://upload.wikimedia.org/wikipedia/commons/2/26/Dice-6-b.svg"
+        ][number];
+
+        result.innerHTML = "🎉 You rolled <b>" + number + "</b>!";
+
+        dice.style.transform = "rotate(0deg)";
+        dice.style.opacity = "1";
+
+    },700);
 
 }
