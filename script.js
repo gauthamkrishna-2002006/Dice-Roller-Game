03da2c8ee42f46c782a593a5498f9ab916a1c20c
gauthamkrishna-2002006 <gauthamkrishnacg@gmail.com>
2026-07-12 20:54:55 +0530

    Add real dice images
    Updated rollDice function to display dice image and result.

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-function rollDice() {
-    const diceFaces = ["⚀","⚁","⚂","⚃","⚄","⚅"];
+function rollDice(){
 
-    const number = Math.floor(Math.random() * 6);
+const number=Math.floor(Math.random()*6)+1;
 
-    document.getElementById("dice").textContent = diceFaces[number];
+document.getElementById("dice").src=
+"https://upload.wikimedia.org/wikipedia/commons/"+[
+"",
+"1/1b/Dice-1-b.svg",
+"5/5f/Dice-2-b.svg",
+"b/b1/Dice-3-b.svg",
+"f/fd/Dice-4-b.svg",
+"0/08/Dice-5-b.svg",
+"2/26/Dice-6-b.svg"
+][number];
 
-    document.getElementById("result").textContent =
-        "You rolled a " + (number + 1) + "!";
+document.getElementById("result").innerHTML="You rolled <b>"+number+"</b>";
+
 }
